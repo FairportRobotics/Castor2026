81b2be63f6cbf14114cd31ea59041a9d921c2aab
Mike Silva <mike.a.silva@gmail.com>
2026-01-24 15:36:58 -0500

    Initial commit of python files

---
+++ robot.py
@@ -0,0 +1,15 @@
+import magicbot
+import wpilib
+
+class MyRobot(magicbot.MagicRobot):
+
+    def createObjects(self):
+        '''Create motors and stuff here'''
+        pass
+
+    def teleopInit(self):
+        '''Called when teleop starts; optional'''
+
+    def teleopPeriodic(self):
+        '''Called periodically during teleop'''
+        pass
