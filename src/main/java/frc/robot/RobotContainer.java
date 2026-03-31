d98cf15a8dbefdc0aaa084c208a0ca2b514f74da
TheRealGrayden <nmunier65@gmail.com>
2026-03-31 18:59:26 -0400

    turret lock

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,6 +6,7 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.OperatorConstants;
@@ -79,6 +80,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return driveSubsystem.getAutoCommand();
+    return Commands.parallel(Commands.run(()-> {turretSubsystem.homeTurret();}, turretSubsystem),  driveSubsystem.getAutoCommand());
   }
 }
